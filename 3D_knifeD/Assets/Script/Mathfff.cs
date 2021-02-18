678944cd04aeb0aa7736fd9677dced5ac82f49ca
mch0520 <ch052088@gmail.com>
2021-02-18 20:14:23 +0800

    地圖使用

--- 3D_knifeD/Assets/Script/Mathfff.cs
+++ 3D_knifeD/Assets/Script/Mathfff.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 //製作地圖
 public class Mathfff : MonoBehaviour
 {
+    #region 欄位
+    [Header("地圖座標放大倍數")]
+    public float rage;
+
     /// <summary>
     /// 真實地圖
     /// </summary>
@@ -28,86 +32,135 @@ public class Mathfff : MonoBehaviour
         new GameObject ("smallMapD"),
         new GameObject ("smallMapE"),
     };
-    //格子 位置
-    public Vector3[] lattice;
+    [Header("小地圖位置")]
+    public Vector2[] smallMapAV2;
+    public Vector2[] smallMapBV2;
+    public Vector2[] smallMapCV2;
+    public Vector2[] smallMapDV2;
+    public Vector2[] smallMapEV2;
 
-    private Vector2[] smallMapV2;
-    private bool[] mapBool;
-    private bool[] smallMapBool;
-    //地圖大小
+    //父物件
+    public RectTransform canvas;
+    //選擇生成的 地圖名字
+    public string mapName;
+    //小型地圖大小
     public float mapSize;
+    //路障
+    public GameObject forest;
+    #endregion
 
-
-    private void Awake()
+    /// <summary>
+    /// 地圖選項
+    /// </summary>
+    public void MapOptions()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            map[i] = GetComponent<GameObject>();
-            smallMap[i] = GetComponent<GameObject>();
-            smallMapV2[i] = smallMap[i].transform.position;
-        }
+        //小地圖選項 圖層
+        RaycastHit2D map = Physics2D.Raycast(Input.GetTouch(0).position, transform.up, 0.1f, 1 << 10);
+        string mapName = map.transform.name;
     }
 
     private void OnPointerDown()
     {
-        for (int i = 0; i < 5; i++)
+        RaycastHit2D mapLF = Physics2D.Raycast(Input.GetTouch(0).position + new Vector2(-mapSize, mapSize), transform.up, 0.1f);
+        RaycastHit2D mapLB = Physics2D.Raycast(Input.GetTouch(0).position + new Vector2(-mapSize, -mapSize), transform.up, 0.1f);
+        RaycastHit2D mapRF = Physics2D.Raycast(Input.GetTouch(0).position + new Vector2(mapSize, mapSize), transform.up, 0.1f);
+        RaycastHit2D mapRB = Physics2D.Raycast(Input.GetTouch(0).position + new Vector2(mapSize, -mapSize), transform.up, 0.1f);
+
+        //如果 四角沒有物件擋，就生成
+        if (!mapLF || mapLB || mapRF || mapRB)
         {
-            //在哪個小圖旁邊 點擊,固定選項
-            if (Input.GetTouch(0).position.x - smallMapV2[i - 1].x < mapSize || Input.GetTouch(0).position.y - smallMapV2[i - 1].y < mapSize)
+            if (mapName == smallMap[0].name)
             {
-                smallMapBool[i - 1] = true;
+                //產生(小地圖,父物件)
+                GameObject smallA = Instantiate(smallMap[0], canvas);
+                //小地圖的座標=點擊座標
+                smallA.GetComponent<RectTransform>().anchoredPosition = Input.GetTouch(0).position;
+                //紀錄生成後的 座標
+                Vector2[] smallMapAV2 = { (Vector2)(smallA.transform.position) };
             }
-            //在格子上點擊,如果沒有物件 就產生。排整齊的話,任何i格子都是固定j位置,共36個格子。
-            else for (int j = 0; j < 36; j++)
-                {
-                    if (Input.GetTouch(0).position.x - lattice[j - 1].x < mapSize || Input.GetTouch(0).position.y - lattice[j - 1].y < mapSize ||mapBool[j-1]==false)
-                    {
-                        //產生(大地圖跟小地圖,座標,角度)
-                        Instantiate(smallMap[i - 1], lattice[j - 1], transform.rotation);
-                        //200倍的大小
-                        Instantiate(map[i - 1], lattice[j - 1] * 200, transform.rotation);
-                        mapBool[j-1] = true;
-                    }
-                    //點擊 空白處取消 選項 
-                    else smallMapBool[i - 1] = false;
-                }
+            if (mapName == smallMap[1].name)
+            {
+                GameObject smallB = Instantiate(smallMap[1], canvas);
+                smallB.GetComponent<RectTransform>().anchoredPosition = Input.GetTouch(0).position;
+                //紀錄生成後的 座標
+                smallMapBV2 = { (Vector2)(smallB.transform.position) };
+            }
+            if (mapName == smallMap[2].name)
+            {
+                GameObject smallC = Instantiate(smallMap[2], canvas);
+                smallC.GetComponent<RectTransform>().anchoredPosition = Input.GetTouch(0).position;
+                //紀錄生成後的 座標
+                Vector2[] smallMapCV2 = { (Vector2)(smallC.transform.position) };
+            }
+            if (mapName == smallMap[3].name)
+            {
+                GameObject smallD = Instantiate(smallMap[3], canvas);
+                smallD.GetComponent<RectTransform>().anchoredPosition = Input.GetTouch(0).position;
+                //紀錄生成後的 座標
+                Vector2[] smallMapDV2 = { (Vector2)(smallD.transform.position) };
+            }
+            if (mapName == smallMap[4].name)
+            {
+                GameObject smallE = Instantiate(smallMap[4], canvas);
+                smallE.GetComponent<RectTransform>().anchoredPosition = Input.GetTouch(0).position;
+                //紀錄生成後的 座標
+                Vector2[] smallMapEV2 = { (Vector2)(smallE.transform.position) };
+            }
+            if (mapName == "clear")
+            {
+                RaycastHit2D mapDestroy = Physics2D.Raycast(Input.GetTouch(0).position, transform.up, 0,1<<9);
+                Destroy(mapDestroy.transform.gameObject);
+            }
         }
     }
 
-    private void OnPointerUp()
+    private void OnMove()
     {
-        for (int i = 0; i < 36; i++)
+        RaycastHit2D hit2DF = Physics2D.Raycast(Input.GetTouch(0).position, transform.up, 0, 1 << 9);
+        if (hit2DF.transform.parent.tag == "smallMap" || Input.GetTouch(0).position.y < 700)
         {
-            if (mapBool[i-1])
+            //小地圖的座標=點擊座標
+            hit2DF.transform.position = Input.GetTouch(0).position;
+        }
+    }
+
+    private void EnterGame()
+    {
+        //生成大地圖
+        foreach (Vector2 sMap in smallMapAV2)
+        {
+            GameObject maps = Instantiate(map[0]);
+            //地圖座標調整
+            maps.transform.position = new Vector3(sMap.x * rage, sMap.y * rage, 0);
+            //如果小地圖之間的射線 相隔甚遠
+            RaycastHit2D hit2T = Physics2D.Raycast(sMap + new Vector2(-mapSize, mapSize), maps.transform.right, mapSize * 2, 1 << 9);
+            RaycastHit2D hit2B = Physics2D.Raycast(sMap + new Vector2(-mapSize, -mapSize), maps.transform.right, mapSize * 2, 1 << 9);
+            //生成路障
+            if (hit2T.transform.tag != "smallMap")
             {
-                //產生路障
-
-                //如果 地圖(第九層)相接 消除路障
-                RaycastHit2D hit2DF= Physics2D.Raycast(smallMapV2[i-1],smallMap[i-1].transform.up,mapSize*2+1,1<<9);
-                if(hit2DF.transform.parent.name=="前路障")
-                {
-                    Destroy(map[i].transform.GetChild(0));
-                    Destroy(hit2DF.transform.parent);
-                }
-                if (hit2DF.transform.parent.name == "後路障")
-                {
-                    Destroy(map[i].transform.GetChild(1));
-                    Destroy(hit2DF.transform.parent);
-                }
-                RaycastHit2D hit2DR = Physics2D.Raycast(smallMapV2[i - 1], smallMap[i - 1].transform.right, mapSize * 2 + 1, 1 << 9);
-                if (hit2DR.transform.parent.name == "左路障")
-                {
-                    Destroy(map[i].transform.GetChild(2));
-                    Destroy(hit2DR.transform.parent);
-                }
-                if (hit2DR.transform.parent.name == "右路障")
-                {
-                    Destroy(map[i].transform.GetChild(3));
-                    Destroy(hit2DR.transform.parent);
-                }
-
+                Instantiate(forest, maps.transform);
+                maps.transform.position += new Vector3(0, mapSize, 0);
+            }
+            if (hit2B.transform.tag != "smallMap")
+            {
+                Instantiate(forest, maps.transform);
+                maps.transform.position -= new Vector3(0, mapSize, 0);
+            }
+            RaycastHit2D hit2R = Physics2D.Raycast(sMap + new Vector2(mapSize, -mapSize), maps.transform.up, mapSize * 2, 1 << 9);
+            RaycastHit2D hit2L = Physics2D.Raycast(sMap + new Vector2(-mapSize, -mapSize), maps.transform.up, mapSize * 2, 1 << 9);
+            //生成路障
+            if (hit2R.transform.tag != "smallMap")
+            {
+                Instantiate(forest, maps.transform);
+                maps.transform.position += new Vector3(mapSize, 0, 0);
+            }
+            if (hit2L.transform.tag != "smallMap")
+            {
+                Instantiate(forest, maps.transform);
+                maps.transform.position -= new Vector3(mapSize, 0, 0);
             }
         }
+
 
     }
 
